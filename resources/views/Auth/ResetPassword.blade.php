60979b11d8d47617599d9068f30ceea3e5b42b86
Thuan Nguyen <thuanday2222@gmail.com>
2024-03-21 10:44:39 +0700

    update

--- resources/views/Auth/ResetPassword.blade.php
+++ resources/views/Auth/ResetPassword.blade.php
@@ -2,7 +2,11 @@
 <html lang="en">
 
 <head>
-
+    <title>Web S-Office</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="W S-Office">
+    <meta name="keywords" content="HTML5, CSS3, JavaScript">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @include('Layout.partials.head-css')
 </head>
